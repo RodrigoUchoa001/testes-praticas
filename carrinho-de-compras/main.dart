b37c579d0412c83f1a280b0c1d1f97f44eb43f02
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-09-26 20:56:15 -0300

    executando novo teste

--- carrinho-de-compras/main.dart
+++ carrinho-de-compras/main.dart
@@ -3,4 +3,5 @@ import 'maior-preco-test.dart';
 void main() {
   MaiorPrecoTest teste = MaiorPrecoTest();
   teste.deveRetornarZeroSeCarrinhoVazio();
+  teste.deveRetornarValorDoItemSeCarrinhoCom1Elemento();
 }
