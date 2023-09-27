c29c660dbb3c108aa1dab384e8754d962094ede7
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-09-26 21:14:16 -0300

    rodando testes

--- carrinho-de-compras/main.dart
+++ carrinho-de-compras/main.dart
@@ -4,4 +4,5 @@ void main() {
   MaiorPrecoTest teste = MaiorPrecoTest();
   teste.deveRetornarZeroSeCarrinhoVazio();
   teste.deveRetornarValorDoItemSeCarrinhoCom1Elemento();
+  teste.deveRetornarMaiorValorSeCarrinhoContemMuitosElementos();
 }
